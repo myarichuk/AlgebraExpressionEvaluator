ad6c58a34ae348645504f922a2413074a0269bf9
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-12-17 10:35:27 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Interpreter;
+using Parser;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = string.Empty;
+            Console.WriteLine("Algebra Expression Evaluator");
+            Console.WriteLine("ANTLR4 Parser Demo");
+            Console.WriteLine("=============================");
+            while (input.ToLower() != "q" && input.ToLower() != "quit")
+            {
+                Console.Write("Enter algebraic expression:");
+                input = Console.ReadLine();
+
+                Console.Write($"Result = {Calculate(input)}");
+                Console.WriteLine();
+            }
+        }
+
+        private static BigInteger Calculate(string expression)
+        {
+            var lexer = new AlgebraLexer(new AntlrInputStream(expression));
+            var parser = new AlgebraParser(new CommonTokenStream(lexer));
+
+            var ast = parser.root();
+            var evaluationVisitor = new ExpressionEvaluationVisitor();
+            return evaluationVisitor.Visit(ast);
+        }
+    }
+}
---
+++ Interpreter/ExpressionEvaluationVisitor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+using Parser;
+
+namespace Interpreter
+{
+    //walks over the AST and checks for issues
+    public class ExpressionEvaluationVisitor : AlgebraBaseVisitor<BigInteger>
+    {
+        public override BigInteger VisitPowerExpression(AlgebraParser.PowerExpressionContext context) => 
+            BigInteger.Pow(base.Visit(context.num), (int)base.Visit(context.pow));
+
+        public override BigInteger VisitNumberExpression(AlgebraParser.NumberExpressionContext context) => 
+            BigInteger.Parse(context.GetText(), NumberStyles.Any);
+
+        public override BigInteger VisitMultOrDivideExpression(AlgebraParser.MultOrDivideExpressionContext context) =>
+            context.op.Text switch
+            {
+                "*" => (base.Visit(context.left) * base.Visit(context.right)),
+                "/" => (base.Visit(context.left) / base.Visit(context.right)),
+                _ => throw new ArgumentException($"Unsupported operator '{context.op.Text}'")
+            };
+
+        public override BigInteger VisitPlusOrMinusExpression(AlgebraParser.PlusOrMinusExpressionContext context) =>
+            context.op.Text switch
+            {
+                "+" => (base.Visit(context.left) + base.Visit(context.right)),
+                "-" => (base.Visit(context.left) - base.Visit(context.right)),
+                _ => throw new ArgumentException($"Unsupported operator '{context.op.Text}'")
+            };
+
+        public override BigInteger VisitNegativeNumberExpression(AlgebraParser.NegativeNumberExpressionContext context) => 
+            -1 * base.Visit(context.num);
+
+
+        protected override BigInteger AggregateResult(BigInteger aggregate, BigInteger nextResult) => 
+            aggregate + nextResult;
+    }
+}
